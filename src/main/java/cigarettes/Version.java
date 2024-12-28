de9ff6a5b96a0d0d7e3415961fd5bb9d45841fe4
vx-clutch <owestness@gmail.com>
2024-12-28 09:30:51 -0500

    save

--- src/main/java/cigarettes/Version.java
+++
@@ -1,8 +0,0 @@
-package cigarettes;
-
-public class Version {
-  public static final String MOD_ID = "cigarettes";
-  public static final String MOD_NAME = "Cigarettes";
-  
-  public static final String CHANNEL = MOD_ID;
-}
