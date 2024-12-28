809cc2452f62c04c20b195716090d48654a45991
Ponkoncoma <FireMage2763@gmail.com>
2024-12-28 14:40:15 -0500

    added stuff to Items class, item not finished

--- src/main/java/com/cartman/cigarettes/item/Items.java
+++ src/main/java/com/cartman/cigarettes/item/Items.java
@@ -1,4 +1,20 @@
 package com.cartman.cigarettes.item;
 
+import com.cartman.cigarettes.Cigarettes;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.EventBus;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
 public class Items {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, Cigarettes.MODID);
+
+    public static final RegistryObject<Item> cigar = ITEMS.register("cigar",
+            () -> new Item(new Item.Properties()));
+
+public static void register(IEventBus EventBus) {ITEMS.register(EventBus);}
+
 }
