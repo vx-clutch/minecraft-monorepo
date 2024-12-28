d487eb9f30f5febdee1c15f3aa30d90c430ebcf7
vx-clutch <owestness@gmail.com>
2024-12-28 09:17:15 -0500

    not sure if valid java cuz lsp not working :(

---
+++ src/main/java/cigarettes/Version.java
@@ -0,0 +1,8 @@
+package cigarettes;
+
+public class Version {
+  public static final String MOD_ID = "cigarettes";
+  public static final String MOD_NAME = "Cigarettes";
+  
+  public static final String CHANNEL = MOD_ID;
+}
